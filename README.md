82e44fbe5d1bbf88e70164785c5cbe08b659b6cb
jjdxmashl <316988670@qq.com>
2015-12-05 14:39:53 +0800

    项目构建

---
+++ README.md
@@ -0,0 +1,2 @@
+# jjdxmdownload
+自定义的jjdxmdownload断点下载使用说明
---
+++ app/src/main/java/com/dou361/test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dou361.test;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dou361.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
